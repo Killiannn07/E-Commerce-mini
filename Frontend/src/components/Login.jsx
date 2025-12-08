da8cff44d48954dfa1b9050eebcf9e5d2794d5f2
Killiannn07 <sofyanyunianto.work@gmail.com>
2025-12-08 19:50:06 +0700

    add login page

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,26 +1,57 @@
-import './Login.css';
-import { FaUser, FaUnlock} from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { FaUser, FaUnlock } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const canSubmit = email.trim() !== "" && password.trim() !== "";
   return (
-    <div className="bg-[#3F72AF] p-8 rounded-xl shadow-lg max-w-md w-md">
-        <form className='flex flex-col gap-4'>
-            <h1 className='text-3xl font-bold text-center mb-6'>Login</h1>
-            <div className="justify-center">
-                <input className='w-full px-4 py-2 border rounded-lg' type="email" placeholder='E-mail' required/>
-                <FaUser/>
-            </div>
-            <div className="justify-center">
-                <input className='w-full px-4 py-2 border rounded-lg' type="password" placeholder='Password' required/>
-                <FaUnlock/>
-            </div>
-            <button type="submit" className="p-6">Login</button>
-            <div className="justify-center">
-                <p className='justify-center text-center'>Don't have an account? <a href="#">Register</a></p>
-            </div>
-
-        </form>
-
-
+    <div className="bg-transparent px-6 py-12 rounded-xl shadow-2xl w-sm border border-white backdrop-blur-lg">
+      <form className="flex flex-col">
+        <h1 className="text-3xl font-bold text-center mb-10 text-white">
+          Login
+        </h1>
+        <div className="relative w-full mb-8">
+          <input
+            className="w-full h-full pl-4 pr-8 py-4 border border-white rounded-2xl  outline-none text-white"
+            type="text"
+            placeholder="E-mail or Username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <FaUser className="absolute right-3 top-1/2 -translate-y-1/2 text-white" />
+        </div>
+        <div className="relative w-full mb-8">
+          <input
+            className="w-full h-full pl-4 pr-8 py-4 border border-white rounded-2xl  outline-none text-white"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <FaUnlock className="absolute right-3 top-1/2 -translate-y-1/2 text-white" />
+        </div>
+        <button
+          type="submit"
+          className="py-3 px-1 bg-white w-1/2 m-4 mx-auto block font-bold rounded-2xl cursor-pointer" 
+          onClick={() => canSubmit && navigate("/")}
+          
+        >
+          Login
+        </button>
+        <div className="justify-center">
+          <p className="text-center text-white">
+            Don't have an account?{" "}
+            <a className="text-white underline font-bold" href="#">
+              Register
+            </a>
+          </p>
+        </div>
+      </form>
     </div>
-);}
+  );
+}
